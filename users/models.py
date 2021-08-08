e87afbd4a9ac2b2636f743e0504d494c0a968577
pial_noman <awronno.adhar@gmail.com>
2021-08-08 11:09:38 +0600

    VO-1

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
-
 from .managers import CustomUserManager
 
 
@@ -15,6 +14,7 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
+    phone = models.CharField(_('phone number'), max_length=15, blank=True)
     date_of_birth = models.DateField(blank=True, null=True)
     first_name = models.CharField(_('first name'), max_length=150, blank=True)
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
