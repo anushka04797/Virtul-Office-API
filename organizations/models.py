cb2872a55f741d4f0669b7c53c12785b74189d86
pial_noman <awronno.adhar@gmail.com>
2021-08-12 11:44:27 +0600

    VO-2

--- organizations/models.py
+++ organizations/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django import forms
 from django.utils.translation import ugettext_lazy as _
 from users.models import CustomUser
 
@@ -19,10 +18,10 @@ class Company(models.Model):
 
 
 class Department(models.Model):
-    company_id = models.ForeignKey(Company, blank=False, null=False, on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, blank=False, null=False, on_delete=models.PROTECT)
     name = models.CharField(_('department name'), max_length=150, blank=False)
     details = models.TextField(_('department details'), max_length=350, blank=True)
-    parrent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.PROTECT)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'department'
@@ -34,10 +33,10 @@ class Department(models.Model):
 
 
 class Designation(models.Model):
-    department_id = models.ForeignKey(Department, blank=False, null=False, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, blank=False, null=False, on_delete=models.PROTECT)
     name = models.CharField(_('designation name'), max_length=150, blank=False)
     details = models.TextField(_('designation details'), max_length=350, blank=True)
-    parrent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.PROTECT)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'designation'
@@ -50,17 +49,20 @@ class Designation(models.Model):
 
 class HolidayPlan(models.Model):
     WEEK_DAYS = (
-        ('Saturday','Saturday'),
+        ('Saturday', 'Saturday'),
         ('Sunday', 'Sunday'),
-        ('Monday','Monday'),
-        ('Tuesday','Tuesday'),
-        ('Wednesday','Wednesday'),
-        ('Thursday','Thursday'),
-        ('Friday','Friday'),
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday', 'Friday'),
     )
-    week_start_day = models.CharField(_('Select week start day'), max_length=20, choices=WEEK_DAYS, default='Select', blank=False, null=False)
-    weekholiday_1 = models.CharField(_('Select week holiday 1'), max_length=20, choices=WEEK_DAYS, default='Select', blank=False, null=False)
-    weekholiday_2 = models.CharField(_('Select week holiday 2'), max_length=20, choices=WEEK_DAYS, default='Select', blank=False, null=True)
+    week_start_day = models.CharField(_('Select week start day'), max_length=20, choices=WEEK_DAYS, default='Select',
+                                      blank=False, null=False)
+    week_holiday_1 = models.CharField(_('Select week holiday 1'), max_length=20, choices=WEEK_DAYS, default='Select',
+                                      blank=False, null=False)
+    week_holiday_2 = models.CharField(_('Select week holiday 2'), max_length=20, choices=WEEK_DAYS, default='Select',
+                                      blank=False, null=True)
     government_holidays = models.IntegerField(_('government holidays'), blank=True, null=True)
     company_reserved_holidays = models.IntegerField(_('company\'s reserved holidays'), blank=True, null=True)
     allocated_leave_per_year = models.IntegerField(_('allocated leave per head in a year'), blank=True, null=True)
@@ -76,18 +78,18 @@ class HolidayPlan(models.Model):
 
 class Calender(models.Model):
     MONTHS = (
-        ('January','January'),
+        ('January', 'January'),
         ('February', 'February'),
-        ('March','March'),
-        ('April','April'),
-        ('May','May'),
-        ('June','June'),
-        ('July','July'),
-        ('August','August'),
-        ('September','September'),
-        ('October','October'),
-        ('November','November'),
-        ('December','December'),
+        ('March', 'March'),
+        ('April', 'April'),
+        ('May', 'May'),
+        ('June', 'June'),
+        ('July', 'July'),
+        ('August', 'August'),
+        ('September', 'September'),
+        ('October', 'October'),
+        ('November', 'November'),
+        ('December', 'December'),
     )
     VACATION_TYPE = (
         ('Government', 'Government'),
@@ -96,9 +98,10 @@ class Calender(models.Model):
     Year = models.CharField(_('Year'), max_length=20, blank=False, null=False)
     Month = models.CharField(_('Month'), max_length=20, choices=MONTHS, default='Select', blank=False, null=False)
     vacation_title = models.CharField(_('vacation title'), max_length=120, blank=False, null=False)
-    vacation_type = models.CharField(_('select vacation type'), max_length=20, choices=VACATION_TYPE, default='Select', blank=False, null=True)
-    start_data = models.DateField(_('company\'s reserved holidays'), blank=True, null=True)
-    end_date = models.DateField(_('company\'s reserved holidays'), blank=True, null=True)
+    vacation_type = models.CharField(_('select vacation type'), max_length=20, choices=VACATION_TYPE, default='Select',
+                                     blank=False, null=True)
+    start_data = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
     duration = models.DurationField()
 
     class Meta:
@@ -111,9 +114,9 @@ class Calender(models.Model):
 
 
 class Slc(models.Model):
-    employee_id = models.ForeignKey(CustomUser, blank=False, null=False, on_delete=models.PROTECT)
-    department_id = models.ForeignKey(Department, blank=False, null=False, on_delete=models.PROTECT)
-    designation_id = models.ForeignKey(Designation, blank=False, null=False, on_delete=models.PROTECT)
+    employee = models.ForeignKey(CustomUser, blank=False, null=False, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, blank=False, null=False, on_delete=models.PROTECT)
+    designation = models.ForeignKey(Designation, blank=False, null=False, on_delete=models.PROTECT)
     salary = models.IntegerField(_('salary'), blank=False, null=False)
     hourly_rate = models.IntegerField(_('hourly rate'), blank=False, null=False)
 
@@ -123,4 +126,4 @@ class Slc(models.Model):
         verbose_name_plural = _('slcs')
 
     def __str__(self):
-        return self.employee_id.first_name + " " + self.employee_id.last_name
+        return self.employee.first_name + " " + self.employee.last_name
