a7caaa4f805adff127b82775dece59ef28894de8
saiforahi <shaif.rahi@northsouth.edu>
2022-03-20 13:32:09 +0600

    projects data building in search same as project details

--- search/views.py
+++ search/views.py
@@ -11,8 +11,9 @@ from rest_framework.views import APIView
 
 from meetings.models import Meetings
 from meetings.serializers import MeetingsDetailsSerializer
-from projects.models import Projects
-from projects.serializers import ProjectDetailsSerializer
+from projects.models import Projects, ProjectAssignee
+from projects.serializers import ProjectDetailsSerializer, TaskSerializer, ProjectAssigneeSerializer
+from projects.views import unique
 from users.models import CustomUser
 from users.serializers import UserDetailSerializer
 
@@ -21,7 +22,6 @@ class Search(APIView):
     permission_classes = (AllowAny,)
 
     def get(self, request):
-
         try:
             models = {
                 'user': 'CustomUser',
@@ -31,9 +31,10 @@ class Search(APIView):
             users = UserDetailSerializer(CustomUser.objects.filter(Q(first_name__icontains=key) | Q(last_name__icontains=key)), many=True).data
             projects = ProjectDetailsSerializer(Projects.objects.filter(Q(sub_task__icontains=key) | Q(task_title__icontains=key)), many=True).data
             #meetings = MeetingsDetailsSerializer(Meetings.objects.filter(Q(sub_task__icontains=key) | Q(task_title__icontains=key)), many=True).data
+
             result = {
                 'employees':users,
-                'projects':projects
+                'projects':build_projects_data(projects)
             }
 
             response = {
@@ -52,3 +53,41 @@ class Search(APIView):
             print(str(e))
 
         return Response(response)
+
+
+def build_projects_data(projects):
+    projects_data = []
+    traversed_projects = []
+    for project in projects:
+        print(project['id'])
+        temp_project = Projects.objects.get(pk=project['id'])
+        if temp_project.work_package_number not in traversed_projects:
+            traversed_projects.append(temp_project.work_package_number)
+            subtask_query_set = Projects.objects.filter(work_package_number=temp_project.work_package_number)
+            # print('subtasks',subtask_query_set)
+            subtasks = []
+            assignees = []
+            if len(subtask_query_set) > 0:
+                for task in subtask_query_set:
+                    serialized_task = TaskSerializer(task).data
+                    temp_assignees = ProjectAssigneeSerializer(ProjectAssignee.objects.filter(project=task.id),
+                                                               many=True).data
+                    serialized_task['assignees'] = temp_assignees
+                    subtasks.append(serialized_task)
+                    for assignee in temp_assignees:
+                        assignees.append(
+                            UserDetailSerializer(CustomUser.objects.get(pk=assignee['assignee']['id'])).data)
+
+            unique_assignees = unique(assignees)
+            serializer = ProjectDetailsSerializer(temp_project)
+            # print(assignees)
+            temp_data = {
+                'assignees': unique_assignees,
+                'project': serializer.data,
+                'subtasks': subtasks,
+                'sub_task': serializer.data['sub_task']
+            }
+            projects_data.append(temp_data)
+
+    return projects_data
+
